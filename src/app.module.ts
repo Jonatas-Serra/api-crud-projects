1ec588b609afc666a87bb6852d72591de1f61aa9
Jonatas Serra <jonatasserra@outlook.com>
2022-09-29 10:42:03 -0300

    refatoração e adição de .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProductsModule } from './products/products.module'
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://jonatas:sVQa6Ea6t9ADT90J@cluster0.j29ist0.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERMONGO}:${process.env.SECRETMONGO}@crudapp.eiecace.mongodb.net/?retryWrites=true&w=majority`,
     ),
     ProductsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = process.env.PORT || 3000
-
-  app.enableCors()
   await app.listen(PORT, () =>
     console.log(`Server is listening on port ${PORT}...`),
   )
